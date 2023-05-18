fc418206d7564cb7f38f336449e294faa6d0312c
nathan <nathanmageka6@gmail.com>
2023-05-18 11:12:45 +0300

    nathan

--- Swapping.rb
+++ Swapping.rb
@@ -2,15 +2,13 @@
     Write a function variable_swap that takes two variables 
     a and b as input and swaps their values.
 =end
-
 def variable_swap(a, b)
-    # Your code here
-end
+    return b, a
+  end
   
-# Example usage
-a = 5
-b = 10
-variable_swap(a, b)
-puts a # Expected output: 10
-puts b # Expected output: 5
-  
+  # Example usage
+  a = 5
+  b = 10
+  a, b = variable_swap(a, b)
+  puts a # Expected output: 10
+  puts b # Expected output: 5
